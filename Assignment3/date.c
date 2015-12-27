c46964596374acda9f60248285ef8dae24b93621
DavyVan <affqqa@163.com>
2015-12-27 21:26:40 +0800

    Add Assignment 3

---
+++ Assignment3/date.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  struct rtcdate r;
+
+  if (date(&r) == 0) {
+    printf(2, "date failed\n");
+    exit();
+  }
+
+  // your code to print the time in any format you like...
+  printf(2, "current time: %d-%d-%d %d:%d:%d\n", r.year, r.month, r.day, r.hour, r.minute, r.second);
+
+  exit();
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # OS-Engineering-Assignments
 ## Contents
 HW1. A simple shell exercise  
-HW2. A interpretation of call stack initialized by boot loader
+HW2. A interpretation of call stack initialized by boot loader  
+HW3. A new system call 'date'  
